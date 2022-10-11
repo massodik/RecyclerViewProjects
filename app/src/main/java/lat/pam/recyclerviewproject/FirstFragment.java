b72c58392cdc09dccea3e430afea46dd4ea8dcdc
aqsal <muhammadaqsal96@gmail.com>
2022-10-11 19:41:09 +0700

    first commit

---
+++ app/src/main/java/lat/pam/recyclerviewproject/FirstFragment.java
@@ -0,0 +1,79 @@
+package lat.pam.recyclerviewproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.LinkedList;
+
+import lat.pam.recyclerviewproject.databinding.FragmentFirstBinding;
+
+
+public class FirstFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private FragmentFirstBinding binding;
+    final LinkedList<String> mWordList = new LinkedList<>();
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Put initial data into the word list.
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+        // Get a handle to the RecyclerView.
+        mRecyclerView = view.findViewById(R.id.recyclerview);
+
+         // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+                // Add a new word to the wordList.
+                mWordList.addLast("+ Word " + wordListSize);
+                // Notify the adapter, that the data has changed.
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                // Scroll to the bottom.
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/lat/pam/recyclerviewproject/WordListAdapter.java
@@ -0,0 +1,74 @@
+package lat.pam.recyclerviewproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>{
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        public WordViewHolder(@NonNull View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+
+        public void onClick(View v) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    public WordListAdapter(FirstFragment context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context.getContext());
+        this.mWordList = wordList;
+    }
+
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate an item view.
+        View mItemView = mInflater.inflate(
+                R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+
+
+
+}
